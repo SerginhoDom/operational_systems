813464cfac6370c8ff55576d880ca112cdb7ec18
Sergey Domashev <106580245+SerginhoDom@users.noreply.github.com>
2025-01-21 15:49:53 +1000

    Delete first directory

--- first/bash.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd hello_world
-mkdir build & cd build
-cmake -G "MinGW Makefiles" ..
-cmake --build .
-helloworld.exe
--- first/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir build & cd build
-cmake ../hello_world
-cmake -- build .
-./hello_world
